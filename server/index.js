8f46ca0306013ff9b07d79a61de660c2495fd6f5
Tomas Jakelis <tomasjakelis@gmail.com>
2022-02-03 20:24:10 +0000

    feat(Module 1): completes module

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-const port = 8070;
+require("dotenv").config({
+  path: path.join(__dirname, "..", ".env.server")
+});
+
+const port = process.env.PORT;
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
